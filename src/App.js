efd2650c650ae50d4b687f2d3c9d764aef8f1457
ziyadmerhebi <ziyadmerhebi@gmail.com>
2019-01-19 16:59:26 -0600

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom'; 
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,10 +15,13 @@ class App extends Component {
     return (
       <React.Fragment>
         <Navbar />
-        <ProductList />
-        <Details />
-        <Cart />
-        <Default />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        
       </React.Fragment>
     );
   }
