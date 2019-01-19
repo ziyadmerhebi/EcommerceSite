11b5ae403a3d89ec6d79d385ed9aeb00853ff620
ziyadmerhebi <ziyadmerhebi@gmail.com>
2019-01-19 16:33:39 -0600

    Initial framework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import Default from './components/Default';
+import Details from './components/Details';
+import ProductList from './components/ProductList';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <ProductList />
+        <Details />
+        <Cart />
+        <Default />
+      </React.Fragment>
     );
   }
 }
